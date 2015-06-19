02ab5e50fbe8b7bb461b8ae82997f0de8efc6459
uelipeter <ueli.peter@comerge.net>
2015-06-19 15:08:42 +0200

    Made some progress

--- src/scope.js
+++ src/scope.js
@@ -3,28 +3,75 @@
 
 function Scope() {
     this.$$watchers = [];
+    this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+function initWatchVal() { }
+
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
     var watcher = {
         watchFn: watchFn,
-        listenerFn: listenerFn
+        listenerFn: listenerFn || function() { },
+        valueEq: !!valueEq,
+        last: initWatchVal
     };
     this.$$watchers.push(watcher);
+    this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$digest = function() {
+    var ttl = 10;
+    this.$$lastDirtyWatch = null;
+    while (this.$$digestOnce() && ttl > 0){ttl--;}
+    if (ttl <= 0){throw "digest ttl reached: to many iterrations!";}
+};
+
+
+Scope.prototype.$$digestOnce = function() {
     var self = this;
-    var newValue, oldValue;
+    var newValue, oldValue, dirty;
     _.forEach(this.$$watchers, function(watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
-        if (newValue !== oldValue) {
-            watcher.last = newValue;
-            watcher.listenerFn(newValue, oldValue, self);
+        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+            self.$$lastDirtyWatch = watcher;
+            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+            if (oldValue === initWatchVal){
+                watcher.listenerFn(newValue, newValue, self);
+            }else {
+                watcher.listenerFn(newValue, oldValue, self);
+            }
+            dirty = true;
+        } else if (self.$$lastDirtyWatch === watcher) {
+            return false;
         }
     });
+    return dirty;
 };
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+    if (valueEq) {
+        return _.isEqual(newValue, oldValue);
+    } else {
+        return newValue === oldValue || (typeof newValue === 'number' && typeof oldValue === 'number' && isNaN(newValue) && isNaN(oldValue));
+    }
+};
+
+Scope.prototype.$eval = function(expr, locals) {
+    return expr(this, locals);
+};
+
+Scope.prototype.$apply = function(expr) {
+    try {
+        return this.$eval(expr);
+    } finally {
+        this.$digest();
+    }
+};
+
+
+
+
 
 
 
