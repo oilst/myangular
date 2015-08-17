ae1d97e6f7d9b3212d19cf647c53c63bcafd9851
Ueli Peter <uelipeter@Uelis-MacBook-Pro.local>
2015-08-17 22:04:16 +0200

    tarted with watching collections

--- src/scope.js
+++ src/scope.js
@@ -275,6 +275,29 @@ Scope.prototype.$destroy = function () {
     }
 };
 
+Scope.prototype.$watchCollection = function(watchFn, listenerFn)
+{
+    var self = this;
+    var newValue;
+    var oldValue;
+    var changeCount = 0;
+
+    var internalWatchFn = function(scope) {
+        newValue = watchFn(scope);
+        if (!self.$$areEqual(newValue, oldValue, false)) {
+            changeCount++;
+        }
+        oldValue = newValue;
+        return changeCount;
+    };
+
+    var internalListenerFn = function() {
+        listenerFn(newValue, oldValue, self);
+    };
+    return this.$watch(internalWatchFn, internalListenerFn);
+};
+
+
 
 
 
