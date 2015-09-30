5163ad7d6390c0b06c92dbe555e52d47f3ce797e
uelipeter <ueli.peter@comerge.net>
2015-09-30 14:47:57 +0200

    started with parser

---
+++ test/parse_spec.js
@@ -0,0 +1,48 @@
+/* jshint globalstrict: true */ /* global parse: false */
+'use strict';
+
+describe("parse", function () {
+    it("can parse an integer", function () {
+        var fn = parse('42');
+        expect(fn).toBeDefined();
+        expect(fn()).toBe(42);
+    });
+    it("can parse a floating point number", function () {
+        var fn = parse('4.2');
+        expect(fn()).toBe(4.2);
+    });
+
+    it("can parse a floating point number without an integer part", function () {
+        var fn = parse('.42');
+        expect(fn()).toBe(0.42);
+    });
+    it("can parse a number in scientific notation", function () {
+        var fn = parse('42e3');
+        expect(fn()).toBe(42000);
+    });
+    it("can parse a floating point number in scientific notation", function () {
+        var fn = parse('.42e2');
+        expect(fn()).toBe(42);
+    });
+    it("can parse a number in scientific notation with negative exponent", function () {
+        var fn = parse('4200e-2');
+        expect(fn()).toBe(42);
+    });
+    it("can parse a number in scientific notation with positive exponent", function () {
+        var fn = parse('.42e+2');
+        expect(fn()).toBe(42);
+    });
+    it("can parse a number in scientific notation with capital E", function () {
+        var fn = parse('.42E2');
+        expect(fn()).toBe(42);
+    });
+
+    it("will not parse invalid scientific notation", function () {
+        expect(function () {
+            parse('42e-');
+        }).toThrow();
+        expect(function () {
+            parse('42e-a');
+        }).toThrow();
+    });
+});
